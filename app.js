816dc122b20373ce8cb0ff35a23833bd700dbda7
Olivier Poisson <70164489+olivierpoisson@users.noreply.github.com>
2022-02-03 20:04:44 -0500

    Carousel Animations

--- app.js
+++ app.js
@@ -71,3 +71,35 @@ const timelineSplitPin = gsap.timeline({
         end: "100%",
     },
 });
+
+//! Carousel
+const swatches = document.querySelectorAll(".swatches img");
+const gallery = document.querySelector(".phone-gallery");
+const slides = document.querySelectorAll(".phone-gallery-container");
+
+let currentSwatch = "blue";
+let topIndex = 2;
+
+swatches.forEach((swatch, index) => {
+    const coord = slides[index].getBoundingClientRect().left;
+    swatch.addEventListener("click", (e) => {
+        let swatchName = e.target.getAttribute("swatch");
+        let closeUp = document.querySelector("." + swatchName);
+
+        //* Check if we are already on the swatch
+        if (currentSwatch === swatchName) {
+            return;
+        }
+
+        gsap.set(closeUp, { zIndex: topIndex });
+        gsap.fromTo(closeUp, { opacity: 0 }, { opacity: 1, duration: 1 });
+
+        //* Gallery
+        gsap.to(gallery, { x: -coord, duration: 1, ease: "back.out(1)" });
+
+        //? Increment zindex
+        topIndex++;
+        //? Set currentSwatch
+        currentSwatch = swatchName;
+    });
+});
